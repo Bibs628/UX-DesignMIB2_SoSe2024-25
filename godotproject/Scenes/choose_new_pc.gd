c321c4de3fd8a3bb38e7e8af203e2cbe2706b673
Daniel B. Georg <124447854+SootDan@users.noreply.github.com>
2025-01-21 15:56:58 +0100

    sample animation/sound

--- godotproject/Scenes/choose_new_pc.gd
+++ godotproject/Scenes/choose_new_pc.gd
@@ -5,16 +5,23 @@ func _ready() -> void:
 	$Main/Myself.connect("pressed", hover)
 	$Main/PreBuild.connect("pressed", prebuild)
 	$TextureRect/Button.connect("pressed", back)
+	$AnimationPlayer.play("Click")
 
 func hover():
+	$AudioStreamPlayer.play()
+	OS.delay_msec(50)
 	var loading_screen = LoadingScreen.new()
 	loading_screen.target_scene = "res://Scenes/PrebuildConfig.tscn"
 	get_tree().change_scene_to_file("res://Scenes/LoadingScreen.tscn")
 	
 func prebuild():
+	$AudioStreamPlayer.play()
+	OS.delay_msec(50)
 	var loading_screen = LoadingScreen.new()
 	loading_screen.target_scene = "res://Scenes/PrebuildSelection.tscn"
 	get_tree().change_scene_to_file("res://Scenes/LoadingScreen.tscn")
 
 func back():
+	$AudioStreamPlayer.play()
+	OS.delay_msec(50)
 	get_tree().change_scene_to_file("res://Scenes/Welcome.tscn")
